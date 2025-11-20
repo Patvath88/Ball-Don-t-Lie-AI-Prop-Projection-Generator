60d47a6c7ed6161abdf7dd702b0a6d4277733842
Patvath88 <patvath8@gmail.com>
2025-11-20 16:52:55 -0500

    Update features.py

--- utils/features.py
+++ utils/features.py
@@ -1,1 +1,13 @@
+# utils/features.py
 
+def get_feature_columns():
+    return [
+        "pts_last5",
+        "reb_last5",
+        "ast_last5",
+        "min_last5",
+        "pts_avg",
+        "reb_avg",
+        "ast_avg",
+        "min_avg"
+    ]
